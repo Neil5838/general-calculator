96f5e5d6e2e152d424e14402e1155f4a218e5394
Neil5838 <neilSE5838@gmail.com>
2025-02-04 21:51:31 +0545

    all done

---
+++ README.md
@@ -0,0 +1,2 @@
+General calculator with basic operations!
+Hope you guys like it. Happy coding!
---
+++ src/main.js
@@ -0,0 +1,41 @@
+const display = document.getElementById('display');
+display.value = 0;
+
+const keys = document.getElementsByClassName('key');
+Array.from(keys).forEach(key => {
+    key.addEventListener('click', (e) => {
+        const keyValue = e.target.textContent;
+
+        if(keyValue === 'AC') {
+            display.value = 0;
+            return
+        }
+
+        if(keyValue === 'DE') {
+            display.value = display.value.length > 1 ? display.value.slice(0, -1) : 0;
+            return
+        }
+
+        if(keyValue === '=') {
+            try {
+                display.value = display.value.replaceAll(/X/g, '*');
+                display.value = eval(display.value);
+            } catch (error) {
+                display.value = 'Invalid input';
+            }
+            return
+        }
+
+        if(/[+-\/X.]/.test(keyValue) && /[+-\/X.]$/.test(display.value)) {
+            return
+        }
+
+       if(display.value === '0' && /\d/.test(keyValue)) {
+        display.value = keyValue;
+       } else {
+        display.value += keyValue;
+       }
+    })
+});
+
+
